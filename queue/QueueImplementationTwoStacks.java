cce7a4675493739d4027951d14ed4c76314a6fd2
lakshan chathura <37142357+lakshan-chathura@users.noreply.github.com>
2021-08-05 08:02:55 +0530

    Update QueueImplementationTwoStacks.java

--- queue/QueueImplementationTwoStacks.java
+++ queue/QueueImplementationTwoStacks.java
@@ -1,48 +1,44 @@
+public class QueueUsingTwoStacks {
+    Stack<Integer> stack1=new Stack<>();
+    Stack<Integer> stack2=new Stack<>();
 
-package queue;
-
-//Method 1
-
-import java.util.Stack;
-
-// By making enqueue opearton costly.
-class Queue{
-    static Stack<Integer> stack1=new Stack<>();
-    static Stack<Integer> stack2=new Stack<>();
-    
-    static void enqueue(int x){
-        
-        stack1.push(x);
+    public void enqueque(int element){
+        stack1.push(element);
     }
-    static int dequeue(){
-        
-        if(stack1.isEmpty() && stack2.isEmpty())
-            throw new IllegalStateException("Stack is Empty!!");
-        
+    public int dequeue(){
+        if(isEmpty()){
+            throw new IllegalStateException();
+        }
         if(stack2.isEmpty()){
             while(!stack1.isEmpty()){
-                  stack2.push(stack1.pop());
+                stack2.push(stack1.pop());
             }
-              
         }
         return stack2.pop();
     }
+    public int peek(){
+        if(isEmpty()){
+            throw new IllegalStateException();
+        }
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
 
-}
-
-public class QueueImplementationTwoStacks
-{
+    public boolean isEmpty(){
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
 
     public static void main(String[] args) {
-        
-        Queue.enqueue(1);
-        Queue.enqueue(2);
-        Queue.enqueue(3);
-  
-        System.out.println(Queue.dequeue());
-        System.out.println(Queue.dequeue());
-        System.out.println(Queue.dequeue());
+        QueueUsingTwoStacks queueUsingTwoStacks=new QueueUsingTwoStacks();
+        queueUsingTwoStacks.enqueque(10);
+        queueUsingTwoStacks.enqueque(20);
+        queueUsingTwoStacks.enqueque(30);
+
+
     }
+
 }
-
-
